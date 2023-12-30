f13cb33da1bb2ca123ad3b47d9d6fec8efc4267e
Saugat-Gr <fangurung90@gmail.com>
2023-12-30 09:20:50 +0545

    Update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,7 +43,7 @@
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                      <span style="margin-right:150px;">  {{ __('Forgot your password?') }} </span>
+                      <span style="margin-right:120px;">  {{ __('Forgot your password?') }} </span>
                     </a> <br>
                 @endif
             </div>
